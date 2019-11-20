dec4fc1178923c6f533d2b6faa55c79a34701783
unknown <vinicius.silveira8@hotmail.com>
2019-11-20 19:30:54 -0300

    Commit inicial

---
+++ src/main/java/com/desafio/senior/SeniorApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.senior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/senior/SeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.senior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
