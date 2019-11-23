ead57710577fae7e8d0da258db3e4b0d2613ff90
unknown <vinicius.silveira8@hotmail.com>
2019-11-23 11:37:21 -0300

    Adição de biblioteca de JSON Continuação do desenvolvimento dos desafios

--- src/main/java/com/desafio/senior/controller/CidadeController.java
+++ src/main/java/com/desafio/senior/controller/CidadeController.java
@@ -1,11 +1,14 @@
 package com.desafio.senior.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.desafio.senior.model.Cidade;
 import com.desafio.senior.repository.CidadeRepository;
 import com.desafio.senior.service.CidadeService;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,8 +17,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -51,25 +54,26 @@ public class CidadeController {
     return cidadeRepository.save(cidade);
   }
 
-  @PutMapping(value = "/cidades/{ibgeid}")
-  public ResponseEntity<Cidade> put(@PathVariable(value = "ibgeid") int ibgeId, @RequestBody Cidade cidadeNew) {
-    Optional<Cidade> cidadeOld = cidadeRepository.findByIbgeId(ibgeId);
+  // @PutMapping(value = "/cidades/{ibgeid}")
+  // public ResponseEntity<Cidade> put(@PathVariable(value = "ibgeid") int ibgeId,
+  // @RequestBody Cidade cidadeNew) {
+  // Optional<Cidade> cidadeOld = cidadeRepository.findByIbgeId(ibgeId);
 
-    if (cidadeOld.isPresent()) {
-      Cidade cidade = cidadeOld.get();
-      int idOld = cidade.getIdcidade();
+  // if (cidadeOld.isPresent()) {
+  // Cidade cidade = cidadeOld.get();
+  // int idOld = cidade.getIdcidade();
 
-      // Atualizando objeto
-      cidade = cidadeNew;
-      // Mantendo o mesmo id do objeto original
-      cidade.setIdcidade(idOld);
-      cidadeRepository.save(cidade);
+  // // Atualizando objeto
+  // cidade = cidadeNew;
+  // // Mantendo o mesmo id do objeto original
+  // cidade.setIdcidade(idOld);
+  // cidadeRepository.save(cidade);
 
-      return new ResponseEntity<Cidade>(cidade, HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-  }
+  // return new ResponseEntity<Cidade>(cidade, HttpStatus.OK);
+  // } else {
+  // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  // }
+  // }
 
   @DeleteMapping(value = "/cidades/{ibgeid}")
   public ResponseEntity<Object> Delete(@PathVariable(value = "ibgeid") int ibgeId) {
@@ -84,7 +88,12 @@ public class CidadeController {
     }
   }
 
-  @GetMapping(value = "/cidades/importacao")
+  @GetMapping(value = "/pesquisa/cidades/capital")
+  public List<Cidade> getCapitais() {
+    return cidadeRepository.findByCapitalTrueOrderByName();
+  }
+
+  @PostMapping(value = "/cidades/importacao")
   public ResponseEntity<Object> importarDados() {
     try {
       cidadeService.setupCidades();
@@ -93,5 +102,73 @@ public class CidadeController {
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @GetMapping(value = "/pesquisa/count/cidades/uf/maiormenor")
+  public ResponseEntity<String> retornaCountMaiorMenorCidadesPorUf() {
+    List<Object[]> countCidadesPorUf = cidadeRepository.retornaCountCidadesPorUf();
+
+    if (!countCidadesPorUf.isEmpty()) {
+      JsonArray array = new JsonArray();
+      JsonObject cidadeMaior = new JsonObject();
+      JsonObject cidadeMenor = new JsonObject();
+
+      cidadeMaior.addProperty("uf", countCidadesPorUf.get(0)[0].toString());
+      cidadeMaior.addProperty("count", countCidadesPorUf.get(0)[1].toString());
+
+      cidadeMenor.addProperty("uf", countCidadesPorUf.get(countCidadesPorUf.size() - 1)[0].toString());
+      cidadeMenor.addProperty("count", countCidadesPorUf.get(countCidadesPorUf.size() - 1)[1].toString());
+
+      array.add(cidadeMaior);
+      array.add(cidadeMenor);
+
+      return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping(value = "/pesquisa/count/cidades/uf")
+  public ResponseEntity<String> countCidadesPorUf() {
+    List<Object[]> countCidadesPorUf = cidadeRepository.retornaCountCidadesPorUf();
+
+    if (!countCidadesPorUf.isEmpty()) {
+      JsonArray array = new JsonArray();
+      JsonObject object = new JsonObject();
+
+      for (Object[] dados : countCidadesPorUf) {
+        object = new JsonObject();
+        object.addProperty("uf", dados[0].toString());
+        object.addProperty("count", dados[1].toString());
+
+        array.add(object);
+      }
+
+      return new ResponseEntity<String>(array.toString(), HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping(value = "/cidades/name")
+  public ResponseEntity<?> cidadesNamePorUf(@RequestParam(value = "uf") String uf) {
+    List<Cidade> cidadesPorUf = cidadeRepository.findAllByUf(uf.toUpperCase());
+
+    if (!cidadesPorUf.isEmpty()) {
+      List<String> cidadesName = new ArrayList<String>();
+
+      cidadesPorUf.stream().forEach(cidade -> cidadesName.add(cidade.getName()));
+
+      return new ResponseEntity<>(cidadesName, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping(value = "/pesquisa/count/cidades")
+  public int countCidadesPorColuna(@RequestParam(value = "coluna") String coluna) {
+    int countCidadesPorColuna = cidadeRepository.countRegistrosPorColuna(coluna);
+
+    return countCidadesPorColuna;
   }
 }
--- src/main/java/com/desafio/senior/repository/CidadeRepository.java
+++ src/main/java/com/desafio/senior/repository/CidadeRepository.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import com.desafio.senior.model.Cidade;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -18,4 +20,13 @@ public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
   Optional<Cidade> findByIbgeId(int ibgeId);
 
   List<Cidade> findByCapitalTrueOrderByName();
+
+  @Query(value = "SELECT uf, COUNT(idcidade) AS count" + " FROM cidade " + " GROUP BY uf "
+      + " ORDER BY count DESC", nativeQuery = true)
+  List<Object[]> retornaCountCidadesPorUf();
+
+  List<Cidade> findAllByUf(@Param(value = "uf") String uf);
+
+  @Query(value = "SELECT COUNT(DISTINCT(:coluna)) FROM cidade", nativeQuery = true)
+  int countRegistrosPorColuna(@Param(value = "coluna") String coluna);
 }
